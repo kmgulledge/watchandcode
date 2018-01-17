9e677d786ef1b615cca316a0d058518c9637fb58
kmgulledge <kmgulledge@gmail.com>
2018-01-16 22:09:06 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # watchandcode
+Here are some examples from the tutorial watch and code. 
---
+++ sandwich.js
@@ -0,0 +1,13 @@
+
+makeSandwichWith
+    Get one slice of bread.
+    Add
+    Put a slice of bread on top.
+
+function makeSandwichWith(filling) {
+    Get one slice of bread;
+    Add filling;
+    Put a slice of bread on top;
+}
+
+makeSandwichWith('ham')
