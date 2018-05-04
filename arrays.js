2f8c800264f772da67a1bfc3781775d06ef58883
kmgulledge <kmgulledge@gmail.com>
2018-05-03 21:42:20 -0400

    upload array

---
+++ arrays.js
@@ -0,0 +1,19 @@
+// create a new, empty array
+var a = [];
+
+// add members to an existing array
+a[0] = 'Kevin';
+a[1] = 'Kendall';
+a[2] = 'Kaleb';
+a[3] = 'Abe';
+a[4] = 'Baby';
+
+// or simply push to the array
+a.push("Fin Fin");
+a.push('Her Royal Fatness');
+a.push('Si Si');
+a.push('Whisper');
+a.push('Mae Mae');
+
+// create a new array and predifine some memebers. 
+var b = ['Bob', 'Courtney', 'Knox', 'Nash', 'JC'];
